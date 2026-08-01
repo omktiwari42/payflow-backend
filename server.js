cdfe53b414d4c53496ce913d752a50f5343b6455
Om Tiwari <omktiwari42@gmail.com>
2026-08-01 16:09:36 +0530

    Configure Express server and authentication routes

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "🚀 PayFlow Backend API Running",
+  });
+});
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
